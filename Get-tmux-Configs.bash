d07e9ecac36e3a8a139fc53830a4dcf7991a3dc4
5aberd2 <93603153+5aberd2@users.noreply.github.com>
2021-12-03 14:43:29 +0200

    Create Get-tmux-Configs.bash

---
+++ Get-tmux-Configs.bash
@@ -0,0 +1,6 @@
+sudo apt install git
+sudo apt install wget
+sudo apt install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/5aberd2/tmux/blob/master/.tmux.conf ~/.tmux.conf
+tmux source ~/.tmux.conf
